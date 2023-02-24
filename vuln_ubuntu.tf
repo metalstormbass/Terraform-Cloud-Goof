53cb4cb67104e8aa714338f27bab79839cb1ec63
mike <michaelbraunbass@gmail.com>
2023-02-24 07:20:29 -0700

    Fixed TF Issue

--- vuln_ubuntu.tf
+++ vuln_ubuntu.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "vuln_vm" {
         network_interface_id = aws_network_interface.tfgoof-nic.id
   }
   
-  user_data = data.template_file.userdata_setup.rendered
+  user_data =  base64encode(templatefile("vuln_bootstrap.sh"))
   tags = {
    Environment = "DEV"
    Owner = var.owner
